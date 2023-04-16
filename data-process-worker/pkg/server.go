53170af5a9e4a2789bfcfb83884cce852fc8cbde
verasthiago <github@verasthiago.com>
2023-04-16 01:41:54 -0700

    Worker server implementation

---
+++ data-process-worker/pkg/server.go
@@ -0,0 +1,48 @@
+package pkg
+
+import (
+	"fmt"
+
+	"github.com/hibiken/asynq"
+	"github.com/verasthiago/verancial/worker/pkg/builder"
+	"github.com/verasthiago/verancial/worker/pkg/handlers"
+	"github.com/verasthiago/verancial/worker/pkg/types"
+)
+
+type Server struct {
+	builder.Builder
+
+	ReportCreateAPI handlers.CreateReportAPI
+}
+
+func (s *Server) InitFromBuilder(builder builder.Builder) *Server {
+	s.Builder = builder
+
+	s.ReportCreateAPI = new(handlers.CreateReportHandler).InitFromBuilder(builder)
+	return s
+}
+
+func (s *Server) Run() error {
+	dsn := fmt.Sprintf("%+v:%+v", s.GetSharedFlags().QueueHost, s.GetSharedFlags().QueuePort)
+	redisConnection := asynq.RedisClientOpt{
+		Addr: dsn,
+	}
+
+	worker := asynq.NewServer(redisConnection, asynq.Config{
+		Concurrency: 10,
+		Queues: map[string]int{
+			"critical": 6,
+			"default":  3,
+			"low":      1,
+		},
+	})
+
+	mux := asynq.NewServeMux()
+
+	mux.HandleFunc(
+		types.PatternReportProcess,
+		s.ReportCreateAPI.Handler,
+	)
+
+	return worker.Run(mux)
+}
