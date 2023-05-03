853d13e8125aa2959933a4e0b3c019c278420f66
verasthiago <github@verasthiago.com>
2023-05-02 20:02:54 -0700

    Temporary queue payload

--- shared/types/payload.go
+++ shared/types/payload.go
@@ -3,6 +3,6 @@ package types
 import "github.com/verasthiago/verancial/shared/constants"
 
 type QueuePayload struct {
-	FilePath string             `json:"filepath"`
-	BankName constants.BankName `json:"bankname"`
+	FilePath string           `json:"filepath"`
+	BankName constants.BankID `json:"bankname"`
 }
