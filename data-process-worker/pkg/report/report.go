c35850369293396562bc6f642e2b0991f89b3753
verasthiago <github@verasthiago.com>
2023-05-02 20:21:32 -0700

    Report interface

--- data-process-worker/pkg/report/report.go
+++ data-process-worker/pkg/report/report.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 
 	"github.com/verasthiago/verancial/shared/constants"
+	"github.com/verasthiago/verancial/shared/models"
+	"github.com/verasthiago/verancial/worker/pkg/report/nubank"
+	"github.com/verasthiago/verancial/worker/pkg/report/scotiabank"
+	"github.com/verasthiago/verancial/worker/pkg/report/wise"
 )
 
 type BankReport interface {
-	ProcessReport(filePath string) error
+	LoadFromCSV(csvPath string) (error, []interface{})
+	Process(bankTransactions []interface{}) (error, []*models.Transaction)
 }
 
-func GetReportProcessor(bankName constants.BankName) (error, BankReport) {
+func GetReportProcessor(bankName constants.BankID) (error, BankReport) {
 	switch bankName {
 	case constants.ScotiaBank:
-		return nil, ScotiaBankReportProcessor{}
+		return nil, scotiabank.ScotiaBankReportProcessor{}
+	case constants.ScotiaBankCC:
+		return nil, scotiabank.ScotiaBankCCReportProcessor{}
+	case constants.Nubank:
+		return nil, nubank.NubankReportProcessor{}
 	case constants.Wise:
-		return nil, WiseReportProcessor{}
+		return nil, wise.WiseReportProcessor{}
 	default:
 		return fmt.Errorf("Bank not supported"), nil
 	}
