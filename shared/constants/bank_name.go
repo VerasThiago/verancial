f13e8ce5f70a25c84c0a5773343e81df43acccd8
verasthiago <github@verasthiago.com>
2023-05-09 18:18:06 -0700

    Fixed bank and app Id

--- shared/constants/bank_name.go
+++ shared/constants/bank_name.go
@@ -1,10 +1,10 @@
 package constants
 
-type BankID int
+type BankId string
 
 const (
-	ScotiaBank BankID = iota
-	ScotiaBankCC
-	Nubank
-	Wise
+	ScotiaBank   BankId = "eb0d5f2b-8b8c-4f9b-8e1a-223e69facbd0"
+	ScotiaBankCC BankId = "01ffa084-3b61-4987-997f-02d07cf86cff"
+	Nubank       BankId = "2462cb1a-f1bd-45c3-b388-da489021d3cd"
+	Wise         BankId = "698aec8d-837a-4fd3-b9df-d22a28ead947"
 )
