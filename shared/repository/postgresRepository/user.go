d5a41273ad7e139e3b7ab5459e8b4a60537f7b79
verasthiago <github@verasthiago.com>
2023-05-09 18:20:27 -0700

    GetUserById Implementation

--- shared/repository/postgresRepository/user.go
+++ shared/repository/postgresRepository/user.go
@@ -13,6 +13,12 @@ func (p *PostgresRepository) GetUserByEmail(email string) (*models.User, error) 
 	return &user, err
 }
 
+func (p *PostgresRepository) GetUserByID(id string) (*models.User, error) {
+	var user models.User
+	err := errors.HandleDataNotFoundError(p.db.Where("id = ?", id).First(&user).Error, USER_DATA_NAME)
+	return &user, err
+}
+
 func (p *PostgresRepository) CreateUser(user *models.User) error {
 	return errors.HandleDuplicateError(p.db.Create(user).Error)
 }
@@ -26,8 +32,8 @@ func (p *PostgresRepository) UpdateUser(user *models.User) error {
 	return errors.HandleDataNotFoundError(p.db.Model(user).Updates(user).Error, USER_DATA_NAME)
 }
 
-func (p *PostgresRepository) DeleteUser(userID string) error {
-	return errors.HandleDataNotFoundError(p.db.Where("id = ?", userID).Delete(&models.User{}).Error, USER_DATA_NAME)
+func (p *PostgresRepository) DeleteUser(userId string) error {
+	return errors.HandleDataNotFoundError(p.db.Where("id = ?", userId).Delete(&models.User{}).Error, USER_DATA_NAME)
 }
 
 func (p *PostgresRepository) MigrateUser(model *models.User) error {
