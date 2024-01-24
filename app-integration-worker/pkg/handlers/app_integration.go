bad30b8298544fba6cb797849aa2dbe0e75766f8
verasthiago <github@verasthiago.com>
2024-01-23 23:05:07 -0300

    Log app-integration steps in console

--- app-integration-worker/pkg/handlers/app_integration.go
+++ app-integration-worker/pkg/handlers/app_integration.go
@@ -40,29 +40,50 @@ func (c *AppIntegrationHandler) Handler(context context.Context, task *asynq.Tas
 		fmt.Printf("Total time %#v seconds\n", time.Since(startTime).Seconds())
 	}(time.Now())
 
+	fmt.Printf("[0/7] Processing request payload...\n")
 	if err = json.Unmarshal(task.Payload()[:], &payload); err != nil {
+		fmt.Printf("\nerr line 44 %+v\n", err)
 		return err
 	}
+	fmt.Printf("\npayload %+v\n", payload)
 
+	fmt.Printf("[1/7] Getting user from database...\n")
 	if user, err = c.GetRepository().GetUserByID(payload.UserId); err != nil {
+		fmt.Printf("\nerr line 51 %+v\n", err)
 		return err
 	}
 
+	fmt.Printf("[2/7] Geting AppReportGenerator...\n")
 	if generator, err = generators.GetAppReportGenerator(payload.AppID); err != nil {
+		fmt.Printf("\nerr line 56 %+v\n", err)
 		return err
 	}
 
-	if lastTransaction, err = generator.GetLastTransaction(user.FinancialAppCredentials[payload.AppID], payload.BankId); err != nil {
+	fmt.Printf("[3/7] Getting user app last transaction...\n")
+	if lastTransaction, err = generator.GetLastTransaction(user.FinancialAppCredentials[payload.AppID], payload.BankId, payload.LastTransactionDate); err != nil {
+		fmt.Printf("\nerr line 61 %+v\n", err)
 		return err
 	}
+	fmt.Printf("\nlastTransaction %+v\n", lastTransaction)
 
+	fmt.Printf("[4/7] Getting all transactions after last from app...\n")
 	if transactions, err = c.GetRepository().GetAllTransactionsFromUserBankAfterDate(user.ID, payload.BankId, lastTransaction); err != nil {
+		fmt.Printf("\nerr line 66 %+v\n", err)
 		return err
 	}
 
+	fmt.Printf("[5/7] Generating App report...\n")
 	if appReport, err = generator.Generate(transactions); err != nil {
+		fmt.Printf("\nerr line 71 %+v\n", err)
 		return err
 	}
 
-	return generator.Submit(user, appReport)
+	fmt.Printf("[6/7] Submiting App report...\n")
+	if err = generator.Submit(user, appReport); err != nil {
+		fmt.Printf("\nerr line 79 %+v\n", err)
+		return err
+	}
+
+	fmt.Printf("[7/7] DONE!\n")
+	return nil
 }
